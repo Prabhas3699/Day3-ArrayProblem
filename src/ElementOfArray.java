92ea3810279d14a3a056f9c022cd993486ed8508
Prabhas3699 <prabhak620@gmail.com>
2022-08-18 18:30:19 +0530

    print the elements of an array

---
+++ src/ElementOfArray.java
@@ -0,0 +1,15 @@
+
+public class ElementOfArray {
+	public static void main(String[] args) {
+        /*
+         * Initialize array
+         */
+        int[] arr ={1,2,3,4,5,6,7};
+        /*
+         * using loop to print multiple values
+         */
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+    }
+}
